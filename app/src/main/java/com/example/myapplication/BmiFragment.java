a28fb5f86af80767d3219f5a34ace9e64b1b2d0a
Your Name <you@example.com>
2023-05-17 10:51:54 +0600

    fragement added

--- app/src/main/java/com/example/myapplication/BmiFragment.java
+++ app/src/main/java/com/example/myapplication/BmiFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +29,8 @@ public class BmiFragment extends Fragment {
 
     public BmiFragment() {
         // Required empty public constructor
+
+
     }
 
     /**
@@ -36,6 +41,7 @@ public class BmiFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment BmiFragment.
      */
+
     // TODO: Rename and change types and number of parameters
     public static BmiFragment newInstance(String param1, String param2) {
         BmiFragment fragment = new BmiFragment();
@@ -53,12 +59,40 @@ public class BmiFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
+
+    View myView;
+    EditText weight, height;
+    TextView result;
+    Button button;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bmi, container, false);
+        myView =  inflater.inflate(R.layout.fragment_bmi, container, false);
+
+        weight = myView.findViewById(R.id.weightEditText);
+        height = myView.findViewById(R.id.heightEditText);
+        result = myView.findViewById(R.id.bmi_result);
+        button = myView.findViewById(R.id.bmiButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateBmi(view);
+            }
+        });
+        return myView;
+    }
+
+    public  void calculateBmi(View view) {
+        double height_value = Double.parseDouble(height.getText().toString());
+        double weight_value = Double.parseDouble(weight.getText().toString());
+        double  res = height_value / (weight_value * weight_value);
+        result.setText("Your BMI is: "+  res);
+
+
     }
 }
